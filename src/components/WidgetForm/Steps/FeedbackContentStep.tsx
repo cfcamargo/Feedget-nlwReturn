8a35532e89bef0136444f86b06774c71221c847c
Cristian Camargo <chris.camargo2015@gmail.com>
2022-05-03 16:44:07 -0400

    Criando botoes do form , e adicionando funcionalidade do screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType,
@@ -33,9 +34,24 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
+      <form action="" className="my-4 w-full">
 
-      </div>
+        <textarea 
+        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" 
+        placeholder="Conte com detalhes o que esta acontecendo"/>
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton />
+
+          <button 
+          type="submit"
+          className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
+
+      </form>
 
     </>
   )

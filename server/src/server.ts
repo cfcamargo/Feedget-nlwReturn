79a3c9680f86e7330433831da116dbd057b87244
Cristian Camargo <chris.camargo2015@gmail.com>
2022-05-04 16:11:18 -0400

    Finalizando o back end, e configurando testes unitarios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,55 +1,15 @@
-import express from 'express';
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma';
+import express, { Router } from 'express';
+import cors from 'cors';
+import { routes } from './routes';
 
 const app = express();
 
 // middleware de uso do json no express
+app.use(cors({
+  origin: 'https://localhost:3000',
+}));
 app.use(express.json());
-
-
-
-//teste mail
-
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "777f19a0470a70",
-    pass: "ee04a67d801e01"
-  }
-});
-
-
-//Routes
-
-app.post('/feedbacks', async (req, res) => {
-
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    }
-  })
-
-  transport.sendMail({
-    from: "Equipe Feedget <oi@fidget.com>",
-    to: 'Cristian Camargo <chris.camargo2015@gmail.com>',
-    subject: "Novo Feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
-      `Tipo do Feedback : ${type} `,
-      `Comentario : ${comment}`,
-      `</div>`
-    ].join('\n'),
-  })
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes)
 
 
 //Config da porta que o servidor vai ouvir
